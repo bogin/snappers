14ea418c16cbc3dd7bc18ebae39157a1b7587d80
shachar bogin <boginlg@gmail.com>
2023-08-05 22:30:37 +0300

    project works for the first time (Can see live facebook video)

--- src/configurations/streams.configurations.ts
+++ src/configurations/streams.configurations.ts
@@ -5,14 +5,14 @@ export const Streams: Stream[] = [
     {
         type: StreamType.FacebookLive,
         data: {
-            video_src: '../public/assets/vidoes/snappers_channel_14.mp4',
+            video_src: './src/public/assets/vidoes/snappers_channel_14.mp4',
             length_in_seconds: 60, // TODO - VALIDATE AGAINST VIDEO LENGTH      
         }
     },
     {
         type: StreamType.FacebookLive,
         data: {
-            video_src: '../public/assets/vidoes/snappers_nba.mp4',
+            video_src: './src/public/assets/vidoes/snappers_nba.mp4',
             length_in_seconds: 60, // TODO - VALIDATE AGAINST VIDEO LENGTH      
         }
     }
