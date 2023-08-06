d34c3c80ce0d5ced1c4e088dd7f1819eff72a3b1
shachar bogin <boginlg@gmail.com>
2023-08-06 20:18:31 +0300

    Add snappers service for readbily of code &context

---
+++ src/services/snappers.service.ts
@@ -0,0 +1,37 @@
+import { cloneDeep } from 'lodash';
+import { Streams } from '../configurations/streams.configurations';
+import { StreamerFactory } from '../factories/streamer.factory';
+import { Stream } from '../models/interfaces/stream.model';
+
+
+export class SnappersService {
+
+    streamsConfigurations: Stream[] = cloneDeep(Streams);
+    streamerFactory = new StreamerFactory();
+
+    constructor() {
+        this.streamsConfigurations = cloneDeep(Streams);
+        this.streamerFactory = new StreamerFactory();
+    }
+
+    startStreamingSequence = () => {
+        let currentTime = 0;
+
+        for (const streamConfiguration of this.streamsConfigurations) {
+            setTimeout(() => this.stream(streamConfiguration), currentTime * 1000);
+            currentTime += streamConfiguration.data.length_in_seconds;
+        }
+    }
+
+
+    stream = (stream: Stream) => {
+        try {
+            const streamer = this.streamerFactory.create(stream);
+            streamer.stream();
+        } catch (e) {
+            console.log('Could not stream', JSON.stringify(stream));
+            console.log('Error: ', e);
+        }
+    }
+
+}
--- src/streamer.ts
+++ src/streamer.ts
@@ -1,26 +1,5 @@
-// import Container from 'typedi';
+import { SnappersService } from "./services/snappers.service";
+
 // TODO REMOVE TYPEDI
-import { StreamerFactory } from './factories/streamer.factory';
-import { Streams } from './configurations/streams.configurations';
-import { cloneDeep } from 'lodash';
-import { Stream } from './models/interfaces/stream.model';
-
-
-const streams: Stream[] = cloneDeep(Streams);
-const streamerFactory = new StreamerFactory();
-
-const startStreamingSequence = () => {
-    let currentTime = 0;
-
-    for (const stream of streams) {
-        setTimeout(() => {
-            console.log(`Starting stream: ${stream.data.video_src}`);
-            const streamer = streamerFactory.create(stream);
-            (streamer as any).stream(stream.data);
-        }, currentTime * 1000);
-
-        currentTime += stream.data.length_in_seconds;
-    }
-}
-
-startStreamingSequence();
+const snappersHomeAssignmentService = new SnappersService();
+snappersHomeAssignmentService.startStreamingSequence();
